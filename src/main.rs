d2a9b722e68e019cf6519b4a8c286e10ed1041dc
Nick Wynja <nick@nickwynja.com>
2023-02-09 09:57:44 -0500

    support local image ref with encoded images

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,23 @@ use std::io::prelude::*;
 use std::env;
 use std::fs::File;
 use open;
-
-// @TODO: https://github.com/webview/webview/issues/44#issuecomment-350342541
-//        Properly load local cid images
+use soup::prelude::*;
+use image_base64;
 
 fn main() {
     if let Some(arg1) = env::args().nth(1) {
         let mut file = File::open(arg1).expect("Unable to open the file");
         let mut contents = String::new();
         file.read_to_string(&mut contents).expect("Unable to read the file");
+
+        let soup = Soup::new(&contents);
+
+        for (_i, link) in soup.tag("img").find_all().enumerate() {
+            let src = link.get("src").expect("Couldn't find link with 'href' attribute");
+            if ! src.starts_with("http") {
+                contents = contents.replace(&src, &image_base64::to_base64(&src));
+            }
+        }
 
         let html = format!(
             r##"
@@ -37,7 +45,7 @@ fn main() {
             "##,
             styles = inline_style(include_str!("./app.css")),
             scripts = inline_script(include_str!("./app.js")),
-            body = contents
+            body = contents,
         );
 
         web_view::builder()
